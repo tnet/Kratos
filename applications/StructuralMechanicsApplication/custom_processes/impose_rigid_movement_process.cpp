28c0635c9aaf5551ee7b26ee53892c38d5583fdc
Vicente Mataix Ferrándiz <vmataix@cimne.upc.edu>
2018-12-07 14:09:21 +0100

    Correct names

--- applications/StructuralMechanicsApplication/custom_processes/impose_rigid_movement_process.cpp
+++ applications/StructuralMechanicsApplication/custom_processes/impose_rigid_movement_process.cpp
@@ -60,14 +60,14 @@ void ImposeRigidMovementProcess::ExecuteInitialize()
     KRATOS_TRY
 
     // Getting model parts
-    ModelPart& root_model_part = mrThisModelPart.GetRootModelPart();
-    ModelPart& model_part = root_model_part.GetSubModelPart(mThisParameters["model_part_name"].GetString());
-    const std::string& new_model_part_name = mThisParameters["new_model_part_name"].GetString();
-    ModelPart& rigid_model_part = new_model_part_name != model_part.Name() ? model_part.HasSubModelPart(new_model_part_name) ? model_part.GetSubModelPart(new_model_part_name) : model_part.CreateSubModelPart(new_model_part_name) : model_part;
+    ModelPart& r_root_model_part = mrThisModelPart.GetRootModelPart();
+    ModelPart& r_model_part = r_root_model_part.GetSubModelPart(mThisParameters["model_part_name"].GetString());
+    const std::string& r_new_model_part_name = mThisParameters["new_model_part_name"].GetString();
+    ModelPart& r_rigid_model_part = r_new_model_part_name != r_model_part.Name() ? r_model_part.HasSubModelPart(r_new_model_part_name) ? r_model_part.GetSubModelPart(r_new_model_part_name) : r_model_part.CreateSubModelPart(r_new_model_part_name) : r_model_part;
 
     // Reorder constrains
     IndexType constraint_id = 1;
-    for (auto& r_constrain : root_model_part.MasterSlaveConstraints()) {
+    for (auto& r_constrain : r_root_model_part.MasterSlaveConstraints()) {
         r_constrain.SetId(constraint_id);
         ++constraint_id;
     }
@@ -75,42 +75,42 @@ void ImposeRigidMovementProcess::ExecuteInitialize()
     // Getting list of variables
     std::vector<Variable<double>> master_double_list_variables, slave_double_list_variables;
     std::vector<VariableComponent<ComponentType>> master_components_list_variables, slave_components_list_variables;
-    const std::string& master_variable_name = mThisParameters["master_variable_name"].GetString();
+    const std::string& r_master_variable_name = mThisParameters["master_variable_name"].GetString();
     // The master variable
-    if(KratosComponents<Variable<double>>::Has(master_variable_name)){
-        Variable<double> variable = KratosComponents<Variable<double>>::Get(master_variable_name);
+    if(KratosComponents<Variable<double>>::Has(r_master_variable_name)){
+        Variable<double> variable = KratosComponents<Variable<double>>::Get(r_master_variable_name);
         master_double_list_variables.push_back(variable);
-    } else if (KratosComponents< VariableComponent<ComponentType>>::Has(master_variable_name)) {
-        VariableComponent<ComponentType> variable = KratosComponents<VariableComponent<ComponentType>>::Get(master_variable_name);
+    } else if (KratosComponents< VariableComponent<ComponentType>>::Has(r_master_variable_name)) {
+        VariableComponent<ComponentType> variable = KratosComponents<VariableComponent<ComponentType>>::Get(r_master_variable_name);
         master_components_list_variables.push_back(variable);
-    } else if (KratosComponents< Variable< array_1d< double, 3> > >::Has(master_variable_name)) {
-        VariableComponent<ComponentType> variable_x = KratosComponents<VariableComponent<ComponentType>>::Get(master_variable_name+"_X");
+    } else if (KratosComponents< Variable< array_1d< double, 3> > >::Has(r_master_variable_name)) {
+        VariableComponent<ComponentType> variable_x = KratosComponents<VariableComponent<ComponentType>>::Get(r_master_variable_name+"_X");
         master_components_list_variables.push_back(variable_x);
-        VariableComponent<ComponentType> variable_y = KratosComponents<VariableComponent<ComponentType>>::Get(master_variable_name+"_Y");
+        VariableComponent<ComponentType> variable_y = KratosComponents<VariableComponent<ComponentType>>::Get(r_master_variable_name+"_Y");
         master_components_list_variables.push_back(variable_y);
-        if (root_model_part.GetProcessInfo()[DOMAIN_SIZE] == 3) {
-            VariableComponent<ComponentType> variable_z = KratosComponents<VariableComponent<ComponentType>>::Get(master_variable_name+"_Z");
+        if (r_root_model_part.GetProcessInfo()[DOMAIN_SIZE] == 3) {
+            VariableComponent<ComponentType> variable_z = KratosComponents<VariableComponent<ComponentType>>::Get(r_master_variable_name+"_Z");
             master_components_list_variables.push_back(variable_z);
         }
     } else {
         KRATOS_ERROR << "Only double, components and vector variables are allowed in the variables list." ;
     }
-    const std::string& slave_variable_name = mThisParameters["slave_variable_name"].GetString();
+    const std::string& r_slave_variable_name = mThisParameters["slave_variable_name"].GetString();
     // We get the slave variable list
-    if (slave_variable_name != "") {
-        if(KratosComponents<Variable<double>>::Has(slave_variable_name)){
-            Variable<double> variable = KratosComponents<Variable<double>>::Get(slave_variable_name);
+    if (r_slave_variable_name != "") {
+        if(KratosComponents<Variable<double>>::Has(r_slave_variable_name)){
+            Variable<double> variable = KratosComponents<Variable<double>>::Get(r_slave_variable_name);
             slave_double_list_variables.push_back(variable);
-        } else if (KratosComponents< VariableComponent<ComponentType>>::Has(slave_variable_name)) {
-            VariableComponent<ComponentType> variable = KratosComponents<VariableComponent<ComponentType>>::Get(slave_variable_name);
+        } else if (KratosComponents< VariableComponent<ComponentType>>::Has(r_slave_variable_name)) {
+            VariableComponent<ComponentType> variable = KratosComponents<VariableComponent<ComponentType>>::Get(r_slave_variable_name);
             slave_components_list_variables.push_back(variable);
-        } else if (KratosComponents< Variable< array_1d< double, 3> > >::Has(slave_variable_name)) {
-            VariableComponent<ComponentType> variable_x = KratosComponents<VariableComponent<ComponentType>>::Get(slave_variable_name+"_X");
+        } else if (KratosComponents< Variable< array_1d< double, 3> > >::Has(r_slave_variable_name)) {
+            VariableComponent<ComponentType> variable_x = KratosComponents<VariableComponent<ComponentType>>::Get(r_slave_variable_name+"_X");
             slave_components_list_variables.push_back(variable_x);
-            VariableComponent<ComponentType> variable_y = KratosComponents<VariableComponent<ComponentType>>::Get(slave_variable_name+"_Y");
+            VariableComponent<ComponentType> variable_y = KratosComponents<VariableComponent<ComponentType>>::Get(r_slave_variable_name+"_Y");
             slave_components_list_variables.push_back(variable_y);
-            if (root_model_part.GetProcessInfo()[DOMAIN_SIZE] == 3) {
-                VariableComponent<ComponentType> variable_z = KratosComponents<VariableComponent<ComponentType>>::Get(slave_variable_name+"_Z");
+            if (r_root_model_part.GetProcessInfo()[DOMAIN_SIZE] == 3) {
+                VariableComponent<ComponentType> variable_z = KratosComponents<VariableComponent<ComponentType>>::Get(r_slave_variable_name+"_Z");
                 slave_components_list_variables.push_back(variable_z);
             }
         } else {
@@ -127,7 +127,7 @@ void ImposeRigidMovementProcess::ExecuteInitialize()
     const int master_node_id = mThisParameters["master_node_id"].GetInt();
 
     // We iterate over the nodes of the rigid model part
-    auto nodes_array = rigid_model_part.Nodes();
+    auto nodes_array = r_rigid_model_part.Nodes();
     const int number_of_nodes = static_cast<int>(nodes_array.size());
 
     // List of variables
@@ -147,7 +147,7 @@ void ImposeRigidMovementProcess::ExecuteInitialize()
         constraints_buffer.reserve(number_of_nodes);
 
         // If we master node ID is zero then we get the first node of the model part
-        NodeType::Pointer p_master_node = (master_node_id == 0) ? *(rigid_model_part.Nodes().begin()).base() : root_model_part.pGetNode(master_node_id);
+        NodeType::Pointer p_master_node = (master_node_id == 0) ? *(r_rigid_model_part.Nodes().begin()).base() : r_root_model_part.pGetNode(master_node_id);
         #pragma omp for
         for (int i = 0; i < number_of_nodes; ++i) {
             auto it_node = nodes_array.begin() + i;
@@ -166,7 +166,7 @@ void ImposeRigidMovementProcess::ExecuteInitialize()
         // We transfer
         #pragma omp critical
         {
-            rigid_model_part.AddMasterSlaveConstraints(constraints_buffer.begin(),constraints_buffer.end());
+            r_rigid_model_part.AddMasterSlaveConstraints(constraints_buffer.begin(),constraints_buffer.end());
             mrThisModelPart.AddMasterSlaveConstraints(constraints_buffer.begin(),constraints_buffer.end());
         }
     }
